4c8ccc515071774a55275cd485db1c21b0289638
DhruvSharma845 <dhruvsharma845@gmail.com>
2021-11-19 21:33:32 +0530

    First model of gauge library

---
+++ src/test_client.cpp
@@ -0,0 +1,160 @@
+#include "test_client.h"
+
+GaugeMethodStore* GaugeMethodStore::pInstance{nullptr};
+
+GaugeMethodStore::GaugeMethodStore() {}
+
+GaugeMethodStore* GaugeMethodStore::GetInstance() {
+    if(pInstance == nullptr) {
+        pInstance = new GaugeMethodStore();
+    }
+    return pInstance;
+}
+
+void GaugeMethodStore::addMethod(GaugeMethodType gaugeMethod) {
+    methods.push_back(gaugeMethod);
+}
+
+std::vector<GaugeMethodMetaData> GaugeMethodStore::getGaugeMethods() {
+    std::vector<GaugeMethodMetaData> gaugesMetaData;
+    int methodIndex = 0;
+    for(auto gaugeMethodPtr: methods) {
+        GaugeMethodMetaData instance{gaugeMethodPtr, methodIndex, 1};
+        gaugesMetaData.push_back(instance);
+        ++methodIndex;
+    }
+    return gaugesMetaData;
+}
+
+/***************************************/
+
+GaugeMethodMetaData::GaugeMethodMetaData(GaugeMethodType gaugeMethod_, int methodIndex_, int numOfThreads_):
+    gaugeMethod(gaugeMethod_),
+    methodIndex(methodIndex_),
+    numOfThreadsToRun(numOfThreads_),
+    numOfIterations(0) {
+
+}
+
+int GaugeMethodMetaData::getConfiguredNumOfThreads() {
+    return numOfThreadsToRun;
+}
+
+GaugeMethodType GaugeMethodMetaData::getGaugeMethod() {
+    return gaugeMethod;
+}
+
+void GaugeMethodMetaData::runGaugeMethod(State& state) {
+    gaugeMethod(state);
+}
+
+int GaugeMethodMetaData::getNumOfIterations() {
+    return numOfIterations;
+}
+
+/***************************************/
+
+GaugeMethodRunner::GaugeMethodRunner(GaugeMethodMetaData gaugeMethodMetaData_, GaugeRunStats gaugeRunStats_):
+   gaugeMethodMetaData(gaugeMethodMetaData_),
+   gaugeRunStats(gaugeRunStats_),
+   iterations((gaugeMethodMetaData.getNumOfIterations() != 0) ? gaugeMethodMetaData.getNumOfIterations() : 1) {
+
+}
+
+void GaugeMethodRunner::execute() {
+    Timer timer{};
+    State state{iterations};
+    gaugeMethodMetaData.runGaugeMethod(state);
+    timer.stopTimer();
+    gaugeRunStats.realTimeUsed = timer.getRealTimeUsed();
+    gaugeRunStats.numOfIterations = iterations;
+}
+
+GaugeRunResult GaugeMethodRunner::getResults() {
+    return GaugeRunResult{gaugeRunStats.realTimeUsed, gaugeRunStats.numOfIterations};
+}
+
+/***********************************/
+
+Timer::Timer():
+    startRealTime(0.0),
+    realTimeUsed(0.0) {
+        startTimer();
+}
+
+void Timer::startTimer() {
+    startRealTime = ChronoClockNow();
+    //std::cout << "Start time -> " << startRealTime << std::endl;
+}
+
+void Timer::stopTimer() {
+    realTimeUsed = ChronoClockNow() - startRealTime;
+    //std::cout << "End time -> " << realTimeUsed << std::endl;
+}
+
+double Timer::getRealTimeUsed() {
+    return realTimeUsed;
+}
+
+double Timer::ChronoClockNow() {
+    using FpSeconds = std::chrono::duration<double, std::chrono::seconds::period>;
+    return FpSeconds(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+}
+
+/************************************/
+
+GaugeRunResult::GaugeRunResult(double realTimeUsed_, int numOfIterations_):
+    realTimeUsed(realTimeUsed_),
+    numOfIterations(numOfIterations_) {
+
+}
+
+double GaugeRunResult::getAverageRealTime() const {
+    return (realTimeUsed / numOfIterations);
+}
+    
+/***********************************/
+
+auto reportResult(const GaugeRunResult& runResult) {
+    std::cout << runResult.getAverageRealTime() << " seconds." << std::endl;
+}
+
+auto registerGaugeMethod(GaugeMethodType gaugeMethod) {
+    GaugeMethodStore::GetInstance()->addMethod(gaugeMethod);
+}
+
+auto runProvidedGaugeMethods() -> void {
+    std::vector<GaugeMethodMetaData> gaugeMethods{GaugeMethodStore::GetInstance()->getGaugeMethods()};
+    if(gaugeMethods.empty()) {
+        std::cout << "Did not find any gauge method to run!!!" << std::endl;
+        return;
+    }
+    std::vector<GaugeMethodRunner> runners;
+    for(const auto& gaugeMethodMetaData: gaugeMethods) {
+        runners.emplace_back(gaugeMethodMetaData, GaugeRunStats());
+    }
+
+    for(auto& runner: runners) {
+        runner.execute();
+        GaugeRunResult runResult = runner.getResults();
+        reportResult(runResult);
+    }
+}
+
+/***********************************/
+
+/**************************/
+
+void Gauge_VectorInit(State& state) {
+    for(int i = 0; i < state.maxIterations(); i++) {
+        std::vector<int> v(state.maxIterations(), 0);
+    }
+    std::cout << "Vector Init" << std::endl;
+}
+
+/**************************/
+
+auto main(int argc, char* argv[]) -> int {
+    registerGaugeMethod(Gauge_VectorInit);
+    runProvidedGaugeMethods();
+}
---
+++ src/test_client.h
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+#include <chrono>
+
+class State {
+public:
+    struct StateIterator {
+        struct Value {};
+        using iterator_category = std::forward_iterator_tag;
+        using value_type = Value;
+        using pointer = Value;  // or also value_type*
+        using reference = Value;  // or also value_type&
+
+        StateIterator(): iterationCount(0) {}
+
+        StateIterator(int iterationCount_): iterationCount(iterationCount_) {}
+
+        Value operator*() {
+            return Value();
+        }
+
+        StateIterator& operator++() {
+            --iterationCount;
+            return *this;
+        }
+
+        bool operator!=(StateIterator const&) const {
+            if(iterationCount != 0) {
+                return true;
+            }
+            return false;
+        }
+
+        int iterationCount;
+    };
+
+    StateIterator begin();
+    StateIterator end();
+
+    int maxIterations() { return iterations; }
+
+    State(int iterations_): iterations(iterations_) {}
+private:
+    int iterations;
+};
+
+using GaugeMethodType = void(*)(State& state);
+
+class GaugeMethodMetaData {
+public:
+    GaugeMethodMetaData(GaugeMethodType gaugeMethod_, int methodIndex_, int numOfThreads_);
+    int getConfiguredNumOfThreads();
+    GaugeMethodType getGaugeMethod();
+    int getNumOfIterations();
+    void runGaugeMethod(State& state);
+private:
+    GaugeMethodType gaugeMethod;
+    int methodIndex;
+
+    int numOfThreadsToRun;
+    int numOfIterations;
+};
+
+class GaugeMethodStore {
+public:
+    void addMethod(GaugeMethodType gaugeMethod);
+    std::vector<GaugeMethodMetaData> getGaugeMethods();
+    static GaugeMethodStore* GetInstance();
+protected:
+     GaugeMethodStore();
+private:
+    GaugeMethodStore(GaugeMethodStore& other) =delete;
+    void operator=(const GaugeMethodStore& other) =delete;
+private:
+    std::vector<GaugeMethodType> methods;
+    static GaugeMethodStore* pInstance;
+};
+
+class GaugeRunStats {
+public:
+    double realTimeUsed;
+    int numOfIterations;
+};
+
+class GaugeRunResult {
+public:
+    GaugeRunResult(double realTimeUsed_, int numOfIterations_);
+    double getAverageRealTime() const;
+private:
+    double realTimeUsed;
+    int numOfIterations;
+};
+
+class GaugeMethodRunner {
+public:
+    GaugeMethodRunner(GaugeMethodMetaData gaugeMethodMetaData_, GaugeRunStats gaugeRunStats_);
+    void execute();
+    GaugeRunResult getResults();
+private:
+    GaugeMethodMetaData gaugeMethodMetaData;
+    GaugeRunStats gaugeRunStats;
+    int iterations;
+};
+
+class Timer {
+public:
+    Timer();
+    void startTimer();
+    void stopTimer();
+    double getRealTimeUsed();
+
+public:
+    double ChronoClockNow();
+    int startRealTime;
+    int realTimeUsed;
+};
